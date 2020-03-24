ab8fe619a45243ce235ba1e8012ae1cee6d4ba75
Kareem Assad <kareemassad5@gmail.com>
2020-03-23 21:43:55 -0400

    finished the validity function.

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-#TODO: Make a function to pick an empty node
+#DONE: Make a function to pick an empty node
 def find_empty_node(board):
     #length of board
     for row in range(len(board)):
@@ -10,9 +10,42 @@ def find_empty_node(board):
     return None
             
 
+#TODO: Make a function to find if the number is valid in the current board
+def check_validity(board, insertion, location):
+    # We need to check the row, column, and square
+    # return FALSE for duplicate, TRUE if we are certain it is valid
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[location[0]][i]  == insertion and location[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        
+            #Checks if any other is equal to the one we just added and that we arent checking the same position twice
+        if board[i][location[1]]  == insertion and location[0] != i:
+            return False
+
+    # Check 3x3 box
+    # This is the value of the rows and columns in each of the 9 quadrants
+    # 
+    # [00,01,02]  
+    # [10,11,12]
+    # [20,21,22]
+    box_Row = location[1] // 3
+    box_Column = location[0] // 3
+
+    for i in range(box_Column * 3, box_Column*3 + 3):
+        for j in range(box_Row * 3, box_Row*3 + 3):
+            #Checks if any other is equal to the one we just added and that we arent checking the same position twice
+            if board[i][j] == insertion and (i,j) != location:
+                return False
+    return True
+
 #TODO: Make a function to try all numbers
 
-#TODO: Make a function to find if the number is valid in the current board
+
 
 #TODO: Make the backtracking function
 
