8bbcf8ba6f7248f002fbd17305acfaf4c5498de7
Kareem Assad <kareemassad5@gmail.com>
2020-03-23 22:38:39 -0400

    the solver is complete. Needs code cleanup and refactoring.

--- sudoku.py
+++ sudoku.py
@@ -43,6 +43,7 @@ def check_validity(board, insertion, location):
                 return False
     return True
 
+# DONE:Implement the backtracking algorithm to solve the board
 def solve_Sudoku(board):
     # recursive backtracking algorithm
     find = find_empty_node(board)
@@ -95,17 +96,18 @@ if __name__ == "__main__":
 
     #Create board to solve
     board = [
-            [5,3,0,0,7,0,0,0,0], 
-            [6,0,0,1,9,5,0,0,0],
-            [0,9,8,0,0,0,0,6,0],
-            [8,0,0,0,6,0,0,0,3],
-            [4,0,0,8,0,3,0,0,1],
-            [7,0,0,0,2,0,0,0,6],
-            [0,6,0,0,0,0,2,8,0],
-            [0,0,0,4,1,9,0,0,5],
-            [0,0,0,0,8,0,0,7,9],
+            [0,0,0,9,0,1,6,0,0], 
+            [5,0,8,0,0,0,0,0,0],
+            [0,0,7,6,0,0,0,5,0], 
+            [0,0,0,0,0,4,0,0,0],
+            [0,0,0,0,0,0,8,0,1],
+            [9,0,6,0,0,0,0,0,0],
+            [0,0,5,0,0,0,4,0,0],
+            [0,0,0,7,0,0,0,0,0],
+            [0,3,0,0,0,9,1,6,2],
         ]
 
+    #Testing
     print_board(board)
     solve_Sudoku(board)
     print("", end="\n")
