b0ba41fb5ddcf905207320909a0b729f44442d8c
Kareem Assad <kareemassad5@gmail.com>
2020-03-31 03:49:04 -0400

    something broke in the enumerate at the end

--- selen.py
+++ selen.py
@@ -47,33 +47,53 @@ while True:
     # Hacky way to detect the numbers on the board as sudoku.com uses SVGS instead of numbers. This checks
     # for height or the special s char at the svg. The dictionary contains 1>5,7,9 and 6,8 are the same size
     # so they use the s char.
-    if (width, height) == (22,32):
-        s = text.find_element_by_tag_name("path").get_attribute("d")[0:6]
-        if s == "M10.53":
-            givenBoard[i].append(8)
-        else:
-            givenBoard[i].append(6)
+        if (width, height) == (22,32):
+            s = text.find_element_by_tag_name("path").get_attribute("d")[0:6]
+            if s == "M10.53":
+                givenBoard[i].append(8)
+            else:
+                givenBoard[i].append(6)
 
 
-    dict = {
-        (12,30) : 1,
-        (20,31) : 2,
-        (21,32) : 3,
-        (24,30) : 4,
-        (21,31) : 5,
-        (20,30) : 7,
-        (23,32) : 9,
-    }
-    givenBoard[i].append(dict.get((width,height)))
+        dict = {
+            (12,30) : 1,
+            (20,31) : 2,
+            (21,32) : 3,
+            (24,30) : 4,
+            (21,31) : 5,
+            (20,30) : 7,
+            (23,32) : 9,
+        }
+        givenBoard[i].append(dict.get((width,height)))
     
-#creating a backup
-backup = list(map(list, givenBoard))
-solve_Sudoku(givenBoard)
+    #creating a backup
+    backup = list(map(list, givenBoard))
+    solve_Sudoku(givenBoard)
 
-print(backup)
-print(givenBoard)
+    print(backup)
+    print(givenBoard)
 
-#using numpad to input
+    #using numpad to input
+    #numpad chain is numpad > numpad-title
+    numpad = driver.find_elements_by_class_name("numpad-title")
 
-#Closes the page
-driver.close()
+    #game input 
+    for yp, y in enumerate(backup):
+        for xp, x in enumerate(y):
+            if x == 0:
+                gameCell[yp*9 + xp].click()
+                numpad[givenBoard[yp][xp] - 1].click()
+        #sometimes a popup blocks it and causes a crash
+        if yp == 6:
+            driver.execute_script("arguments[0].scrollIntoView();", gameTable)
+
+    play_again = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(By.CLASS_NAME, "button-play")
+    )
+
+    ## Comment the bottom 2 lines and the while loop above to make it run only once 
+    time.sleep(2)
+    driver.execute_script("arguments[0].click();", play_again)
+
+    #Closes the page
+    driver.close()
